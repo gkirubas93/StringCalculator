bc3cd63e002700acb598688e45e63ad94923d832
gkirubas93 <gkirubas93@gmail.com>
2024-12-24 00:54:14 +0530

    sixth test passed

--- src/StringCalculator.java
+++ src/StringCalculator.java
@@ -1,3 +1,5 @@
+import java.util.regex.Pattern;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class StringCalculator {
@@ -8,7 +10,14 @@ public class StringCalculator {
 
     public int add(String input) {
         if (input.isEmpty()) return 0;
-        String[] numbers = input.split("[,\\n]");
+        String delimiter = "[,\\n]";
+        if (input.startsWith("//")) {
+            int delimiterIndex = input.indexOf("\\n");
+            delimiter = Pattern.quote(input.substring(2, delimiterIndex));
+            input = input.substring(delimiterIndex + 2);
+        }
+
+        String[] numbers = input.split(delimiter);
         int sum = 0;
         for (String number : numbers) {
             sum += Integer.parseInt(number);
